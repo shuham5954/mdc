64465cbf9c7d8d8b0981a48e189873a3e0d692f5
shubham_vadhariya <vadhariyashubham@gmail.com>
2024-05-12 16:59:40 +0530

    first changies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import {MatChipsModule} from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormLogInComponent } from './form-log-in/form-log-in.component';
+import { ItemContainerComponent } from './item-container/item-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    TopBarComponent,
+    FormLogInComponent,
+    ItemContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatChipsModule,
+    MatFormFieldModule,  // Ensure this is imported
+    MatInputModule,      // If using input fields within mat-form-field
+    FormsModule,         // Optional: for template-driven forms
+    ReactiveFormsModule ,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-log-in/form-log-in.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-log-in',
+  templateUrl: './form-log-in.component.html',
+  styleUrls: ['./form-log-in.component.css']
+})
+export class FormLogInComponent {
+
+}
